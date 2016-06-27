dd44a74526bf551887946762f8d926bb0cfa5096
@cppauluci <cleberson.pauluci@tecnogroup.com.br>
2016-06-27 00:57:09 -0300

    adicionando funcionalidades de exclusao e pesquisaTodos para fabricantes

--- Aulas/Criando projeto do curso/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ Aulas/Criando projeto do curso/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,11 +1,13 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
 import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 public class FabricanteDAO implements Serializable {
@@ -18,6 +20,20 @@ public class FabricanteDAO implements Serializable {
 	@Transactional
 	public void salvar(Fabricante fabricante) {
 		em.persist(fabricante);
+	}
+
+	public List<Fabricante> buscarTodos() {
+		return this.em.createQuery("from Fabricante", Fabricante.class)
+				.getResultList();
+	}
+
+	@Transactional
+	public void excluir(Fabricante fabricante) throws NegocioException {
+		//pesquisando o fabricante antes de excluir
+		//para atachalo no entity manager
+		fabricante = this.em.find(Fabricante.class, fabricante.getCodigo());
+		this.em.remove(fabricante);
+		this.em.flush();
 	}
 	
 }
---
+++ Aulas/Criando projeto do curso/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/PesquisaFabricanteBean.java
@@ -0,0 +1,55 @@
+package com.algaworks.curso.jpa2.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class PesquisaFabricanteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private FabricanteDAO fabricanteDAO;
+	private List<Fabricante> fabricantes;
+	private Fabricante fabricanteSelecionado;
+
+	@PostConstruct
+	public void init() {
+		this.fabricantes = this.fabricanteDAO.buscarTodos();
+	}
+
+	public void excluir() {
+		try {
+			fabricanteDAO.excluir(this.fabricanteSelecionado);
+			this.fabricantes.remove(this.fabricanteSelecionado);
+			FacesUtil.addSuccessMessage("Fabricante " 
+					+ this.fabricanteSelecionado.getNome() 
+					+ " excluído com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage("O fabricante "
+					+ this.fabricanteSelecionado.getNome()
+					+ " não foi excluído!");
+		}
+	}
+	
+	public Fabricante getFabricanteSelecionado() {
+		return fabricanteSelecionado;
+	}
+	public void setFabricanteSelecionado(Fabricante fabricanteSelecionado) {
+		this.fabricanteSelecionado = fabricanteSelecionado;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return this.fabricantes;
+	}
+}
